a959f35f32e6113152bf45274fbc07410a07ff80
WaluigisKitten <114946244+WaluigisKitten@users.noreply.github.com>
2023-02-15 19:52:44 +0000

    fitness upload

---
+++ Forward Fitness/scripts/test.js
@@ -0,0 +1,9 @@
+// JavaScript Document
+function day() {
+	var day = document.getElementById("date");
+	if (day === "saturday") {
+		document.write("I love Saturdays");
+	}  else {
+		document.write("I wish it was Saturday")
+	}
+}
